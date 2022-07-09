26961970cdfc44267220c7a95335c35bc7de639f
Pavel.Koshelev <koshelev_p@mail.ru>
2022-07-09 17:07:18 +0300

    Задача 001

---
+++ 001/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//комментарии
+/*
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine(11%3);
+*/
+
+//С клавиатуры вводится целое число. Вывести квадрат числа
+double a;
+double b;
+string? s=Console.ReadLine();//ввод данных
+a=Convert.ToDouble(s);
+
+b=a*a;//обработак данных
+
+//вывод результата
+System.Console.WriteLine($"{a}^2={b}");//строка интерполяции
+
+
+/*string? s;//объявили переменную
+s=Console.ReadLine();//присвоили переменную
+System.Console.WriteLine(s);
+*/
