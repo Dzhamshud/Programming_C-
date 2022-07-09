70eb4da5b5b808f8714f33d6f31c251fa5c06fd5
Pavel.Koshelev <koshelev_p@mail.ru>
2022-07-09 17:08:36 +0300

    Задача 011

---
+++ 011/Program.cs
@@ -0,0 +1,10 @@
+﻿//Вывести на экран  вторую цифру числа с конца,
+//при условии что число больше 9
+int a=367631659;
+int b=a/10;
+int c=a/10%10;
+//System.Console.WriteLine(b);
+System.Console.WriteLine(c);
+
+
+
