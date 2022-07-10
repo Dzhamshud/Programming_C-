7162b94c45917e72e0cf7ade9c250df8cd8bf79b
Pavel.Koshelev <koshelev_p@mail.ru>
2022-07-10 15:10:25 +0300

    Задание 006_1

--- 006/Program.cs
+++ 006/Program.cs
@@ -1,6 +1,30 @@
 ﻿//Написать программу вычисления значения
 //функции y = sin(a). (Класс Math)
-double x, y;
+/*double x, y;
 x=Math.PI;
 y=Math.Sin(x);
 System.Console.WriteLine($"sin({x})={y}");
+*/
+
+//тип возвращаемого значения Имя_функции(аргумент функции)
+double sin(double x)
+{
+    return Math.Sin(x);
+}
+
+double Sum(double a, double b)
+{
+    return a+b;
+} 
+
+
+
+
+void Pause()
+{
+    System.Console.WriteLine("Press any key");
+}
+
+double y=Sin(3.14);
+System.Console.WriteLine(y);
+Pause();
