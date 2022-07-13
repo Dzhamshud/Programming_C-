8c80f7222eb7d5b2cd2bf96226a3804d1d29c302
Pavel.Koshelev <koshelev_p@mail.ru>
2022-07-13 21:15:35 +0300

    Задача 010

---
+++ 010/Program.cs
@@ -0,0 +1,8 @@
+﻿//Вывести на экран последнюю цифру целого числа введенного с клавиатуры.
+int a, b;
+System.Console.WriteLine("Введите целое число ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+b = a % 10;
+System.Console.Write("Последняя цифра целого числа: ");
+System.Console.WriteLine(b);
