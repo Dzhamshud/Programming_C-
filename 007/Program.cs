0d864f1d28b6e527ee8cfe2b9be5566aebd72332
Pavel.Koshelev <koshelev_p@mail.ru>
2022-07-13 20:44:59 +0300

    Задача 007

---
+++ 007/Program.cs
@@ -0,0 +1,13 @@
+﻿//Выяснить является ли число чётным.
+bool x;
+int number;
+System.Console.WriteLine("Введите число ");
+string? s = Console.ReadLine();
+number = Convert.ToInt32(s);
+x = number % 2 == 0;
+
+if (x)
+{
+    System.Console.WriteLine("Число четное ");
+}
+else System.Console.WriteLine("Число не четное ");
