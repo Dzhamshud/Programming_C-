b11f34cec0f872b5f33cca64849c9ea5ab567e8c
Pavel.Koshelev <koshelev_p@mail.ru>
2022-07-14 22:43:00 +0300

    Задача 013

---
+++ 013/Program.cs
@@ -0,0 +1,11 @@
+﻿//Удалить вторую цифру целого числа введенного с клавиатуры.
+int number, a, b, c;
+System.Console.WriteLine("Введите целое число: ");
+string? s = Console.ReadLine();
+number = Convert.ToInt32(s);
+
+a = number % 10;
+b = number / 10 % 10;
+c = number / 100;
+number = int.Parse(c.ToString()+a.ToString());
+System.Console.WriteLine(number);
