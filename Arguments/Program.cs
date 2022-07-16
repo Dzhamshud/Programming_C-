a221265b5c339470aa2b9a831f7981d22254cea8
Pavel.Koshelev <koshelev_p@mail.ru>
2022-07-16 16:08:08 +0300

    Добавили новые задачи с семинара

---
+++ Arguments/Program.cs
@@ -0,0 +1,12 @@
+﻿//Защита от дурака
+string s;
+int n;
+bool f;
+do
+{
+    s=Console.ReadLine();
+    f=int.TryParse(s,out n);
+    if (f==false) System.Console.WriteLine("Wrong input!");
+}
+while(f==false);
+System.Console.WriteLine(n);
---
+++ VARIABLES/Program.cs
@@ -0,0 +1,16 @@
+﻿//.NET
+//Значимые типы (values types)
+int a;
+double b;
+a=4;
+//ссыдлчные типы (reference types)
+string s;
+s="Hello";
+string s1;
+s1=s;//копирует
+int []arr;
+arr=new int[5];
+arr[0]=1;
+int[]arr2;
+arr2=arr;//присваивает
+arr[0]=2;
