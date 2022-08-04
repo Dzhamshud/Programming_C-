33f35119e38b2d1f51a127d68e2b41f7e2081f50
Pavel.Koshelev <koshelev_p@mail.ru>
2022-08-04 23:28:56 +0300

    Задачи 025 и 027

---
+++ 025/Program.cs
@@ -0,0 +1,32 @@
+﻿//Вывести на экран кубы чисел от 1 до N
+
+int[] array;
+int[] result;
+Init(out array, 8, 1, 9);
+Print(array);
+Slave(array, out result);
+System.Console.WriteLine();
+Print(result);
+
+
+void Init(out int[] array, int Length, int min, int max)
+{
+    array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(min, max + 1);
+}
+
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i],4}");
+}
+
+void Slave(int[] array,out int[] result)
+{
+    result=new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+        result[i]=Convert.ToInt32(Math.Pow(array[i], 3));
+}
+
---
+++ 027/Program.cs
@@ -0,0 +1,10 @@
+﻿//Возведите число А в натуральную степень B используя цикл
+
+int a = 3;
+int b = 4;
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result = result * a;
+}
+System.Console.WriteLine(result);
