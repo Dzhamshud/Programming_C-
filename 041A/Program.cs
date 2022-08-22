99ab1eee72b1010fdaf4ef69c410da03aacbb20e
Pavel.Koshelev <koshelev_p@mail.ru>
2022-08-22 23:15:13 +0300

    Домашняя работа к семинару №6. Задачи 041А и 042А

---
+++ 041A/Program.cs
@@ -0,0 +1,54 @@
+﻿//Дана квадратная матрица.
+//Проверить является ли она симметричной отностительно главной диагонали.
+
+using System;
+{
+    string s = Console.ReadLine();
+    int N = int.Parse(s.Split(' ')[0]);
+    int M = N;
+    int[,] matrix = new int[N, M];
+    bool flag = true;
+
+    CreateArray(matrix);
+    PrintArray(matrix);
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] != matrix[j, i])
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+    if (flag != false) System.Console.WriteLine("Матрица симметрична");
+    else System.Console.WriteLine("Матрица не симметрична");
+
+    void CreateArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            s = Console.ReadLine();
+            string[] ss = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = int.Parse(ss[j]);
+            }
+        }
+    }
+
+    void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                System.Console.Write($"{array[i, j],4} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+}
---
+++ 042A/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Даны две квадратных таблице чисел. Требуется построить третью, каждый
+элемент которой равен сумме элементов, стоящих на том же месте в 1-й и 2-й
+таблицах.
+
+Входные данные
+Во входном файле записано сначала число N, затем записана первая таблица,
+а после нее вторая.Элементы таблиц числа от 0 до 100.
+100>=N>=1
+*/
+
+using System;
+{
+    string s = Console.ReadLine();
+    int N = int.Parse(s.Split(' ')[0]);
+    int M = N;
+    int[,] a = new int[N, M];
+    int[,] b = new int[N, M];
+    int[,] c = new int[N, M];
+
+    CreateArray(a);
+    System.Console.WriteLine();
+    CreateArray(b);
+    System.Console.WriteLine();
+
+    for (int i = 0; i < c.GetLength(0); i++)
+    {
+        for (int j = 0; j < c.GetLength(1); j++)
+        {
+            c[i, j] = a[i, j] + b[i, j];
+        }
+    }
+
+    PrintArray(c);
+
+    void CreateArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            s = Console.ReadLine();
+            string[] ss = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = int.Parse(ss[j]);
+            }
+        }
+    }
+
+    void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                System.Console.Write($"{array[i, j],4} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+}
