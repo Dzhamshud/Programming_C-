2ce1d5af3b23d2372d2aeb85f15bef76fb8bceb6
Pavel.Koshelev <koshelev_p@mail.ru>
2022-08-27 23:42:38 +0300

    Семинар 20.08.2022

---
+++ char/Program.cs
@@ -0,0 +1,15 @@
+﻿char c;
+System.Char c1;
+
+c = 'a';
+System.Console.WriteLine($"{c} {(int)c}");
+for (char c2='a'; c2<='z';c2++)
+{
+    System.Console.WriteLine(c2);
+}
+if (c>='A' && c<='Z')
+    System.Console.WriteLine("Big latin char");
+if (c>='0' && c<='9')
+    System.Console.WriteLine("Char is digit");
+if (char.IsDigit(c))
+    System.Console.WriteLine("Char is digit");
